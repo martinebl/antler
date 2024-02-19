82fe72206adc010ee5b6878d0927a57c2a731a6f
Martin <martin.boruplarsen96@gmail.com>
2024-02-19 09:22:34 +0100

    Add test of clean payload before applying transformations

--- llmtest/harnesses/harness.py
+++ llmtest/harnesses/harness.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from llmtest.generators.gpt4all import GPT4all
-from llmtest.transformers.transformer import Transformer
+from llmtest.transformers import Transformer
 from llmtest.probes import Probe
 
 class Harness:
@@ -15,11 +15,16 @@ class Harness:
     
     def run(self) -> None:
         for probe in self.probes:
-            for transformer in self.transformers:
-                print(probe.getPayload())
-                prompt = transformer.applyExploits(probe.getPayload())
-                answer = self.generator.generate(prompt)
-                print("Prompt: %s" % prompt)
-                print("Answer: %s" % answer)
-                print("Successful hack!") if probe.runDetectors(answer) else print("Failure, the model won")
+            first_answer = self.generator.generate(probe.getPayload())
+            if probe.runDetectors(first_answer):
+                print("Payload was a success in clean form, no need for transformations")
+            else:
+                print("Payload was NOT a success in clean form. Applying transformations...")
+                for transformer in self.transformers:
+                    print(probe.getPayload())
+                    prompt = transformer.applyExploits(probe.getPayload())
+                    answer = self.generator.generate(prompt)
+                    print("Prompt: %s" % prompt)
+                    print("Answer: %s" % answer)
+                    print("Successful hack!") if probe.runDetectors(answer) else print("Failure, the model won")
         
