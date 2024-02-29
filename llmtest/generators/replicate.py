23482e1fe660697ce584abcc9f3f257a8e8d2e74
Martin <martin.boruplarsen96@gmail.com>
2024-02-29 09:41:45 +0100

    Read replicate api key from the .env file

--- llmtest/generators/replicate.py
+++ llmtest/generators/replicate.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import replicate
+from dotenv import load_dotenv
 
 from llmtest.generators import Generator
 
 class Replicate(Generator):
     def __init__(self, model: str, options: dict = {}) -> None:
         super().__init__(model, options)
+        load_dotenv()
+        
 
     def generate(self, prompt: str) -> str:
         ans = ""
