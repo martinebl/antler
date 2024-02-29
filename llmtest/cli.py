21be98839354e697fd3bfd2d29f91be79add041a
Martin <martin.boruplarsen96@gmail.com>
2024-02-29 10:45:13 +0100

    Add full passing of cli arguments with respect to multiprocessing + small cleanup

--- llmtest/cli.py
+++ llmtest/cli.py
@@ -1,10 +1,10 @@
 import argparse
-import sys
+import json
 from llmtest.harnesses.linearharness import LinearHarness
 from llmtest.harnesses.multiprocessharness import MultiProcessHarness
 from llmtest import classfactory
 from llmtest.explorers.exhaustivesearch import ExhaustiveSearch
-from llmtest.generators.gpt4all import GPT4all
+from llmtest.generators.replicate import Replicate
 
 def handle() -> None:
     parser = argparse.ArgumentParser(description="A simple pentesting tool for llm's, using prompt injection attacks")
@@ -44,41 +44,52 @@ def handle() -> None:
         help="Specify which Generator family (class) to use for running the target LLM"
     )
 
+    parser.add_argument(
+        "--options",
+        "-o",
+        type=json.loads,
+        help="Dictionary of options in JSON format"
+    )
+
     args = parser.parse_args()
+
+    # Setting default values
+
+    model = "meta/llama-2-70b-chat" # Default model
+    # model = "orca-mini-3b-gguf2-q4_0" 
 
     probes_path = 'llmtest/probes'
     excluded_probes = ['__init__.py', 'probe.py']
-    probes = []
+    probes = [] # When enough probes are present, it might be too slow to load them all without needing them. Hence the empty start value
 
-    # loads all exploits
+    # loads all exploits (always needed)
     exploits_path = 'llmtest/exploits'
     excluded_exploits = ['__init__.py', 'exploit.py']
     all_exploits = classfactory.instantiate_all_classes_from_folder(exploits_path, excluded_exploits)
 
-    generator_class = GPT4all # Defalt generator
+    generator_class = Replicate # Defalt generator
     generator_path = 'llmtest/generators'
     exluded_generators = ['__init__.py', 'generator.py']
 
-    harness_class = LinearHarness # Default harness
-
     options = {} # Default options
+
+    # Setting given arguments
 
     if args.model:
         model = args.model
     if args.probe:
         probes = classfactory.instantiate_classes_from_folder(probes_path, [args.probe.lower()+'.py'])
+    if not args.probe:
+        # Use all probes if no probe is specified
+        probes = classfactory.instantiate_all_classes_from_folder(probes_path, excluded_probes)
     if args.family:
         generator_class = classfactory.get_classes_from_folder(generator_path, [args.family.lower()+'.py'])[0] # Should only return 1
-    if args.processes:
-        harness_class = MultiProcessHarness
-
-    # Use the default value if either d is specified, or no arguments are given
-    if args.default or len(sys.argv) == 1:
-        model = "orca-mini-3b-gguf2-q4_0"
-        # model = "meta/llama-2-70b-chat"
-        probes = classfactory.instantiate_all_classes_from_folder(probes_path, excluded_probes)
-    elif not args.probe:
-        probes = classfactory.instantiate_all_classes_from_folder(probes_path, excluded_probes)
+    if args.options:
+        options = args.options
     
-    harness = harness_class(probes, ExhaustiveSearch(all_exploits), generator_class, model, options)
+    if args.processes == 1:
+        harness = LinearHarness(probes, ExhaustiveSearch(all_exploits), generator_class, model, options)
+    else:
+        harness = MultiProcessHarness(probes, ExhaustiveSearch(all_exploits), generator_class, model, options, args.processes)
+        
     harness.run()
