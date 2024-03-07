b2adf0cec4c11716493c72f831a888b1be079636
Martin <martin.boruplarsen96@gmail.com>
2024-03-07 13:45:18 +0100

    Add progress bar to the harnesses

--- llmtest/harnesses/linearharness.py
+++ llmtest/harnesses/linearharness.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+from tqdm import tqdm
+
 from llmtest.harnesses import Harness
 from llmtest.probes import Probe
 from llmtest.result import Result
@@ -11,17 +13,17 @@ class LinearHarness(Harness):
         clean_hit_probes: list[Probe] = []
         non_clean_hit_probes: list[Probe] = []
         
-        print("Running penetration tests ...")
+        print("Running penetration tests")
         for i, probe in enumerate(self.probes):
-            print("Checking clean payload for probe %i/%i" % (i+1, len(self.probes)))
+            print("Running test with clean payload for probe %i/%i" % (i+1, len(self.probes)))
             only_payload_answer = self.generator.generate(probe.getPayload())
             clean_hit_probes.append(probe) if probe.runDetectors(only_payload_answer) else non_clean_hit_probes.append(probe)
         
         results.append(Result(Transform([]), len(clean_hit_probes), clean_hit_probes))
 
         if len(non_clean_hit_probes) > 0:
-            for i, transform in enumerate(self.explorer):
-                print("\tTransform %i/%i" % (i+1, len(self.explorer)))
+            print("Runing tests with transforms...")
+            for transform in tqdm(self.explorer):
                 hits = 0
                 for probe in non_clean_hit_probes:
                     prompt = transform.applyExploits(probe.getPayload())
