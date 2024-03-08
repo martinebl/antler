fce83170c9c0af0384cc84265da9567f1a623073
Christoffer Elo Christoffersen <christoffer.elo@outlook.dk>
2024-03-08 15:36:14 +0100

    merged in logging

--- llmtest/harnesses/linearharness.py
+++ llmtest/harnesses/linearharness.py
@@ -34,30 +34,3 @@ class LinearHarness(Harness):
         print("Running penetration tests")
         
         self.evaluateAttempts(self.collectAttempts())
-
-        # results: list[Result] = []
-        # clean_hit_probes: list[Probe] = []
-        # non_clean_hit_probes: list[Probe] = []
-        
-        # print("Running penetration tests")
-        # for i, probe in enumerate(self.probes):
-        #     print("Running test with clean payload for probe %i/%i" % (i+1, len(self.probes)))
-        #     only_payload_answer = self.generator.generate(probe.getPayload())
-        #     clean_hit_probes.append(probe) if probe.runDetectors(only_payload_answer) else non_clean_hit_probes.append(probe)
-        
-        # results.append(Result(Transform([]), len(clean_hit_probes), clean_hit_probes))
-
-        # if len(non_clean_hit_probes) > 0:
-        #     print("Running tests with transforms...")
-        #     for transform in tqdm(self.explorer):
-        #         hits = 0
-        #         for probe in non_clean_hit_probes:
-        #             prompt = transform.applyExploits(probe.getPayload())
-        #             answer = self.generator.generate(prompt)
-        #             if probe.runDetectors(answer): hits += 1
-                    
-        #         result = Result(transform, hits, non_clean_hit_probes)
-        #         results.append(result)
-        #         self.explorer.seedScore(result.score)
-        
-        # self.evaluateResults(results)
