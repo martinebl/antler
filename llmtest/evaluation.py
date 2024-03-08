f66ed0287348a0881d25d77cb1e0310b55638ed6
Christoffer Elo Christoffersen <christoffer.elo@outlook.dk>
2024-03-08 09:25:11 +0100

    removed comment

--- llmtest/evaluation.py
+++ llmtest/evaluation.py
@@ -6,21 +6,7 @@ class Evaluation:
         self.technique_results = technique_results
         self.transform_results = transform_results
         self.probe_results = probe_results
-
-    # def __str__(self) -> str:
-    #     output = "\n--- RESULTS ---"
-    #     output += "\nClean payloads hit: %i" % len(self.clean_hit_payloads)
-        
-    #     for i, clean_payload in enumerate(self.clean_hit_payloads):
-    #         output += "\nPayload %i: '%s'" % (i+1, clean_payload)
-
-    #     if not self.exploit_scores.items(): 
-    #         output += "\nALL PROMPTS HIT CLEAN \nNO EXPLOITS TESTED"
-    #     else:
-    #         for exploit, score_list in self.exploit_scores.items():
-    #             output += "\nExploit: %s - score: %f " % (exploit, sum(score_list) / len(score_list))
-    #     return output
-        
+  
     def __str__(self) -> str:
         output = "\n--- REPORT ---"
 
