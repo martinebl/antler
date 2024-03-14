b42f48edb98a9e1f73ecf7203da1f8a8d4f76490
Martin <martin.boruplarsen96@gmail.com>
2024-03-14 13:57:32 +0100

    Double check the exhaustive search length of transform list

--- llmtest/explorers/exhaustivesearch.py
+++ llmtest/explorers/exhaustivesearch.py
@@ -10,7 +10,7 @@ class ExhaustiveSearch(Explorer):
         super().__init__(techniques)
 
     @staticmethod
-    def generatePermutationsAndCombinations(techniques: list[Technique], min_length: int = 1, max_length: int = 4):
+    def generatePermutationsAndCombinations(techniques: list[Technique], min_length: int = 1, max_length: int = 5):
         combos = [ itertools.combinations(techniques, i) for i in range(min_length, min(len(techniques) + 1, max_length + 1)) ]
         # The list that stores all the permutations, of all the combos
         perms = []
