1442bae6f11581884447905cf588bf6c8a8d74be
Martin <martin.boruplarsen96@gmail.com>
2024-03-14 10:23:07 +0100

    Add gracefull termination on ctrl + c

--- llmtest/harnesses/multiprocessharness.py
+++ llmtest/harnesses/multiprocessharness.py
@@ -1,5 +1,6 @@
 from multiprocessing import Pool
 from tqdm import tqdm
+from signal import signal, SIGINT, SIG_IGN
 
 from llmtest.harnesses import Harness
 from llmtest.explorers.explorer import Explorer
@@ -13,7 +14,11 @@ class MultiProcessHarness(Harness):
     def __init__(self, probes: list[Probe], explorer: Explorer, generator_type: type[Generator], model: str, options: dict = {}, repetitions: int = 1, processes: int = 10) -> None:
         super(MultiProcessHarness, self).__init__(probes, explorer, generator_type, model, options, repetitions)
         self.processes = processes
-
+    
+    @staticmethod
+    def initWorker():
+        # Ignore keyboard interrupts
+        signal(SIGINT, SIG_IGN)
 
     def runCleanProbes(self, pool):
         return pool.map(Harness.runAttempt, [(self, Attempt(Transform([]), probe)) for probe in self.probes] * self.repetitions)
@@ -22,29 +27,32 @@ class MultiProcessHarness(Harness):
         print("Running penetration tests")
         all_attempts: list[Attempt] = []
 
-        with Pool(self.processes) as pool:
+        try:
+            with Pool(self.processes, initializer=MultiProcessHarness.initWorker) as pool:
 
-            print("Running tests with clean payloads...")
-            empty_transform_attempts: list[Attempt] = self.runCleanProbes(pool)
-            empty_transform_attempts = self.collapseSameAttempts(empty_transform_attempts)
-            all_attempts.extend(empty_transform_attempts)
+                print("Running tests with clean payloads...")
+                empty_transform_attempts: list[Attempt] = self.runCleanProbes(pool)
+                empty_transform_attempts = self.collapseSameAttempts(empty_transform_attempts)
+                all_attempts.extend(empty_transform_attempts)
 
-            non_clean_hit_probes = [attempt.getProbe() for attempt in empty_transform_attempts if attempt.getAttemptSuccessRate() != None and attempt.getAttemptSuccessRate() < 1] * self.repetitions
-            
-            if len(non_clean_hit_probes) > 0:
-                print("Running tests with transforms...")
-                pbar = tqdm(self.explorer, leave=False)
-                for transform in pbar:
-                    if self.explorer.hasMessage():
-                        pbar.write(self.explorer.getMessage())
+                non_clean_hit_probes = [attempt.getProbe() for attempt in empty_transform_attempts if attempt.getAttemptSuccessRate() != None and attempt.getAttemptSuccessRate() < 1] * self.repetitions
+                
+                if len(non_clean_hit_probes) > 0:
+                    print("Running tests with transforms...")
+                    pbar = tqdm(self.explorer, leave=False)
+                    for transform in pbar:
+                        if self.explorer.hasMessage():
+                            pbar.write(self.explorer.getMessage())
+                            
+                        transform_attempts: list[Attempt] = pool.map(Harness.runAttempt, [(self, Attempt(transform, probe)) for probe in non_clean_hit_probes])
+                        transform_attempts = self.collapseSameAttempts(transform_attempts)
                         
-                    transform_attempts: list[Attempt] = pool.map(Harness.runAttempt, [(self, Attempt(transform, probe)) for probe in non_clean_hit_probes])
-                    transform_attempts = self.collapseSameAttempts(transform_attempts)
-                    
-                    all_attempts.extend(transform_attempts)
-                    non_failed_attempts = [ attempt for attempt in transform_attempts if attempt.getAttemptSuccessRate() != None]
-                    transform_score = sum([attempt.getAttemptSuccessRate() for attempt in non_failed_attempts ]) / len(non_failed_attempts) if len(non_failed_attempts) != 0 else 0
-                    self.explorer.seedScore(transform_score)
-                    pbar.total = len(self.explorer)
-                pbar.close()
+                        all_attempts.extend(transform_attempts)
+                        non_failed_attempts = [ attempt for attempt in transform_attempts if attempt.getAttemptSuccessRate() != None]
+                        transform_score = sum([attempt.getAttemptSuccessRate() for attempt in non_failed_attempts ]) / len(non_failed_attempts) if len(non_failed_attempts) != 0 else 0
+                        self.explorer.seedScore(transform_score)
+                        pbar.total = len(self.explorer)
+                    pbar.close()
+        except KeyboardInterrupt:
+            print("User interrupted, terminating early")
         return all_attempts
