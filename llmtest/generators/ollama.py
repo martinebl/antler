35ce793b424003e232a27456cdedecb951a245b8
Martin <martin.boruplarsen96@gmail.com>
2024-04-05 08:54:02 +0200

    Add timeout to the ollama client

--- llmtest/generators/ollama.py
+++ llmtest/generators/ollama.py
@@ -8,15 +8,9 @@ class Ollama(Generator):
     """
     def __init__(self, model: str, options: dict = {}) -> None:
         super().__init__(model, options)
-        self.use_custom_client = False
-        if "host" in options.keys():
-            self.use_custom_client = True
-            self.client = ollama.Client(host=options["host"])
+        self.client = ollama.Client(options["host"] if "host" in options.keys() else "127.0.0.1:11434", timeout=30)
 
     def generate(self, prompt:str) -> str:
-        if self.use_custom_client:
-            response = self.client.generate(self.model, prompt)
-        else:
-            response = ollama.generate(self.model, prompt)
+        response = self.client.generate(self.model, prompt)
         return response['response']
 
